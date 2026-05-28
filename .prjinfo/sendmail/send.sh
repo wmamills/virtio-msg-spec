4bafe471195e093c6476fc4276f179e9573ac86b
Bill Mills <bill.mills@linaro.org>
2026-05-28 09:20:43 -0400

    email: enhance send.sh script
    * Ignore this patch
    * don't send to author when sending to --just-me or --just-us
    * eliminate the need for <branch>-patch-base tag
      * Add support for IGNORE_BOTTOM
    * find merge base based on UPSTREAM_BRANCH
    * print the full list of ignored and included commits and get OK
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- .prjinfo/sendmail/send.sh
+++ .prjinfo/sendmail/send.sh
@@ -13,12 +13,65 @@ Viresh Kumar <viresh.kumar@linaro.org>,
 Alex Bennee <alex.bennee@linaro.org>,
 Armelle Laine <armellel@google.com>"
 
+UPSTREAM_BRANCH=master
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-SINCE=${BRANCH}-patch-base
-IGNORE_TOP=1
+IGNORE_TOP=2
+IGNORE_BOTTOM=7     # only used if <branch>-patch-base does not exist
 
 MY_DIR=$(dirname $0)
 BASE_DIR=$(cd $MY_DIR/../..; pwd)
+
+get_tag() {
+    git show-ref --tag --hash $1 2>/dev/null
+}
+
+BASE=$(get_tag ${BRANCH}-merge-base)
+if [ -z "${BASE}" ]; then
+    BASE=$(git merge-base ${UPSTREAM_BRANCH} HEAD)
+fi
+if [ -z "${BASE}" ]; then
+    echo "Can't find base commit (common ancestor)" >&2
+    exit 3
+fi
+
+SINCE=$(get_tag ${BRANCH}-patch-base)
+if [ -z "${SINCE}" ]; then
+    SINCE=$(git log --format=%H ${BASE}..HEAD | tac | head -n $(( $IGNORE_BOTTOM + 0 )) | tail -n 1 )
+fi
+if [ -z "${SINCE}" ]; then
+    echo "Can't find patch base commit (first included commit)" >&2
+    exit 3
+fi
+
+TOP=$(git log -n 1 --format=%H HEAD~${IGNORE_TOP})
+if [ -z "${TOP}" ]; then
+    echo "Can't top commit (last included commit)" >&2
+    exit 3
+fi
+
+if false; then
+    echo "BASE:  $(git log -n 1 --oneline $BASE)"
+    echo "SINCE: $(git log -n 1 --oneline $SINCE)"
+    echo "TOP:   $(git log -n 1 --oneline $TOP)"
+    echo "HEAD:  $(git log -n 1 --oneline HEAD)"
+fi
+
+echo "Ignore top commits:"
+git -P log --oneline ${TOP}..HEAD
+echo
+echo "Include commits:"
+git -P log --oneline ${SINCE}..${TOP}
+echo
+echo "Ignore bottom commits:"
+git -P log --oneline ${BASE}..${SINCE}
+echo
+
+echo "^c to cancel, enter to proceed"
+read
+
+# example commands
+# git log -n 1 --oneline --no-abbrev-commit virtio-msg-patch2-patch-basey
+# git log --oneline --no-abbrev-commit a1883517ee44cc03d1b621a331d24a6f5cc08a92..HEAD | tac | head -n $(( 5 + 1 )) | tail -n 1
 
 case $1 in
 --for-real)
@@ -26,11 +79,12 @@ case $1 in
     ;;
 --just-us)
     TO="$US"
+    EXTRA_SEND_OPTS="--no-signed-off-by-cc --suppress-cc=author"
     ;;
 --just-me)
     TO="$ME"
     CC="$ME2"
-    EXTRA_SEND_OPTS="--no-signed-off-by-cc"
+    EXTRA_SEND_OPTS="--no-signed-off-by-cc --suppress-cc=author"
     ;;
 --dry-run)
     TO="$ME"
